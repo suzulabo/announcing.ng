c44d603e9bc64ad84677c5e6ffccb4e97ee8bed2
kenji suzuki <41730006+suzulabo@users.noreply.github.com>
2022-08-28 22:54:30 +0900

    Add pnpm workspace

--- packages/cloudflare/src/index.ts
+++ packages/cloudflare/src/index.ts
@@ -1,3 +1,7 @@
+import { pathMatcher } from '@announcing/shared';
+
+pathMatcher([], '');
+
 const fetch = async (request: Request) => {
   return Promise.resolve(new Response(`hihi: ${request.url}`));
 };
