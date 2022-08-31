f7d15f560a1563f4b8ba961b9b622c69d59d444a
kenji suzuki <41730006+suzulabo@users.noreply.github.com>
2022-08-31 16:55:01 +0900

    Remove cloudflare is before workspace

--- cloudflare/src/index.ts
+++
@@ -1,5 +0,0 @@
-const fetch = async (request: Request) => {
-  return Promise.resolve(new Response(`hihi: ${request.url}`));
-};
-
-export default { fetch };
--- cloudflare/test/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import handleFetch from '../src/index';
-
-describe('hello', () => {
-  it('basic', async () => {
-    const req = new Request('http://localhost/test');
-    const res = await handleFetch(req);
-    expect(await res.text()).toBe('hihi: http://localhost/test');
-  });
-});
