33550477a56cbd1afd99a519af8aa26c54715a02
kenji suzuki <41730006+suzulabo@users.noreply.github.com>
2022-09-03 07:58:24 +0900

    fix lint

--- packages/cloudflare/src/common/FetchMatch.ts
+++ packages/cloudflare/src/common/FetchMatch.ts
@@ -3,8 +3,8 @@ import { WorkerEnv } from './WorkerEnv';
 
 export type FetchHandler = (
   request: Request,
-  params?: MatchParams,
-  workerEnv?: WorkerEnv
+  params: MatchParams,
+  workerEnv: WorkerEnv
 ) => Response | Promise<Response>;
 
 export type FetchMatch = Match & {
--- packages/cloudflare/src/fetches/index.test.ts
+++ packages/cloudflare/src/fetches/index.test.ts
@@ -1,7 +1,8 @@
+import { WorkerEnv } from '@/common/WorkerEnv';
 import { indexHandler } from '.';
 
 it('basic', async () => {
   const req = new Request('http://localhost/');
-  const res = await indexHandler(req);
+  const res = await indexHandler(req, {}, {} as WorkerEnv);
   expect(await res.text()).toBe('hello');
 });
